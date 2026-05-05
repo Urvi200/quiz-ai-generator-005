9b88b0a1e538862668b0ee8ad68231538ad97df5
Shahil Gangwar <your-email@example.com>
2026-05-05 21:19:19 +0530

    Question Answer System

--- app.py
+++ app.py
@@ -289,4 +289,6 @@ def server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
